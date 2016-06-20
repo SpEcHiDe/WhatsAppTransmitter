d605f135d86752324f6d701f15da636d77576520
maxi <maxi1985.798@gmail.com>
2016-06-20 15:08:45 -0300

    clase AltoSer: es el servicio a correr en background clase AutoArranque: es a donde llega el evento que se encendio el telefono Clase Malware: es en donde hay que poner la magia de nuestro malware

---
+++ app/src/main/java/me/shrimadhavuk/watransmitter/AutoArranque.java
@@ -0,0 +1,16 @@
+package me.shrimadhavuk.watransmitter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by maxi on 19/06/2016.
+ */
+public class AutoArranque extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent = new Intent(context, AltoSer.class);
+        context.startService(serviceIntent);
+    }
+}
